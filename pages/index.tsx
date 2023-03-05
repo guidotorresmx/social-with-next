e939021aad83d829b7501c09522fd3a3a93494bb
Marco Guido <guido-torres@hotmail.com>
2023-03-05 17:39:05 -0500

    setup of base app with firebase logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
+import { limitToLast } from 'firebase/firestore'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
+    
     <div>
-      
+      <Link prefetch={false} href={{
+        pathname: '/[username]',
+        query: {username: 'guidotorresmx'}
+      }}>
+        guidotorresmx's profile
+      </Link>
     </div>
   )
 }
